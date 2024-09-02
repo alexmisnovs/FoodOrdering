5f3ea7666f517b8301844e2e935e83e44b2fa385
Alex Misnovs <aleksandrmisnov@gmail.com>
2024-09-02 12:47:20 +0100

    moved dummy data, implemented dynamic values

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,33 +1,48 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 
 import EditScreenInfo from "@/src/components/EditScreenInfo";
 import { Text, View } from "@/src/components/Themed";
 
+import Colors from "@/src/constants/Colors";
+import products from "../../../assets/data/products";
+
 export default function TabOneScreen() {
+  const product = products[0];
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One Soon to be</Text>
-      <Text style={styles.title}>Tab Two soon to be</Text>
-      <Text style={styles.title}>Tab Two soon to be</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image style={styles.image} source={{ uri: product.image }} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: Colors.light.tabIconDefault,
+    padding: 10,
+    borderRadius: 20,
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
   },
+  image: {
+    width: "100%",
+    aspectRatio: 1
+  },
   title: {
     fontSize: 20,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    marginVertical: 10,
+    color: Colors.light.text
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%"
+  price: {
+    color: Colors.light.tint,
+    fontSize: 16,
+    fontWeight: "bold"
   }
 });
