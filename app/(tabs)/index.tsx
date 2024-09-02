2c0b206590995cc2546d7499dc67903a49bec59a
Alex Misnovs <aleksandrmisnov@gmail.com>
2024-09-02 11:59:16 +0100

    added branch ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ export default function TabOneScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Tab One Soon to be</Text>
       <Text style={styles.title}>Tab Two soon to be</Text>
+      <Text style={styles.title}>Tab Two soon to be</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
