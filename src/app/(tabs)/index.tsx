d8d54c04ef8454c516f37a98b6c513f09d20e82f
Alex Misnovs <aleksandrmisnov@gmail.com>
2024-09-04 14:02:28 +0100

    Playing around

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import ProductListItem from "@components/ProductListItem";
 
 export default function MenuScreen() {
   // const product = products[1];
+  const product2 = products[2];
   //comment
 
   return <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item} />} numColumns={2} contentContainerStyle={{ gap: 10, padding: 10 }} columnWrapperStyle={{ gap: 10 }} />;
