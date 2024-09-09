f0de7f3895cb057d2e0611165ba7a32d65125382
Alex Misnovs <aleksandrmisnov@gmail.com>
2024-09-09 11:42:21 +0100

    added form validation

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,19 +1,44 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
-import React from "react";
+import { useState } from "react";
 import { CURRENCY_SYMBOL } from "@/src/config/general";
 import Button from "@/src/components/Button";
 
 const CreateProductScreen = () => {
-  const [name, setName] = React.useState("");
-  const [price, setPrice] = React.useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [errors, setErrors] = useState("");
 
   const resetForm = () => {
     setName("");
     setPrice("");
+    setErrors("");
+  };
+
+  const validateInput = (): boolean => {
+    setErrors("");
+
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price must be a number");
+      return false;
+    }
+
+    return true;
   };
 
   const onCreate = () => {
-    // TODO form validation
+    if (!validateInput()) {
+      return;
+    }
+
+    // save to the database
     console.log(name, price);
     resetForm();
   };
@@ -26,6 +51,8 @@ const CreateProductScreen = () => {
       {/* Price */}
       <Text style={styles.label}>Price ({CURRENCY_SYMBOL})</Text>
       <TextInput value={price} onChangeText={setPrice} keyboardType="numeric" style={styles.input} placeholder="9.99" />
+      {/* Errors */}
+      <Text style={styles.error}>{errors}</Text>
 
       <Button text="Create" onPress={onCreate} />
     </View>
@@ -52,5 +79,8 @@ const styles = StyleSheet.create({
     color: "gray",
     fontWeight: "bold",
     fontSize: 20
+  },
+  error: {
+    color: "red"
   }
 });
