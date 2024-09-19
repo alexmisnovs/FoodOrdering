b01e27af523f85a49cecf65878e6a61d614d6f05
Alex Misnovs <aleksandrmisnov@gmail.com>
2024-09-19 12:55:20 +0100

    finalised auth

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -72,7 +72,7 @@ export default function AuthProvider({ children }: PropsWithChildren) {
       setLoading(false);
     });
   }, []);
-  // useEffect(() => {
+
   //   supabase.auth
   //     .getSession()
   //     .then(res => {
